3fa7caf840ee23e9d63c83fb8c2df4173a91bf35
Samuel Newman <mozzius@protonmail.com>
2023-09-29 12:46:58 +0100

    Initial Commit

---
+++ Apoapsis/ApoapsisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApoapsisApp.swift
+//  Apoapsis
+//
+//  Created by Samuel Newman on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ApoapsisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
