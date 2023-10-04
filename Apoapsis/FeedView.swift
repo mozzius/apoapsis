0653d2a283dbaf69f8a8a7c6a871e3a755e8d7b0
Samuel Newman <mozzius@protonmail.com>
2023-10-04 16:42:40 +0100

    add repost sheet + images

--- Apoapsis/FeedView.swift
+++ Apoapsis/FeedView.swift
@@ -16,10 +16,13 @@ struct FeedView: View {
         
         List {
             ForEach(vm.posts, id: \.post.uri) { feedPost in
-                FeedViewPostView(feedPost: feedPost)
-                    .task() {
-                        await vm.fetchMore(agent: agent, item: feedPost)
-                    }
+                Section {
+                    FeedViewPostView(feedPost: feedPost)
+                        .listRowInsets(.none)
+                        .task() {
+                            await vm.fetchMore(agent: agent, item: feedPost)
+                        }
+                }
             }
         }.task() {
             await vm.fetch(agent: agent)
@@ -47,6 +50,7 @@ struct FeedView_Previews: PreviewProvider {
 
 struct FeedViewPostView: View {
     var feedPost: ATProto.App.Bsky.Feed.Defs.FeedViewPost
+    @State var repostSheetOpen = false
     
     init(feedPost: ATProto.App.Bsky.Feed.Defs.FeedViewPost) {
         self.feedPost = feedPost
@@ -72,7 +76,7 @@ struct FeedViewPostView: View {
                         Text("@" + feedPost.post.author.handle).foregroundColor(.gray)
                     }
                     
-                }.lineLimit(1).padding(.bottom, 0.5)
+                }.lineLimit(1)
                 
                 if let body = feedPost.post.record.asPost?.text {
                     Text(body)
@@ -80,14 +84,59 @@ struct FeedViewPostView: View {
                         .multilineTextAlignment(.leading)
                 }
                 
-                HStack() {
+                if let embed = feedPost.post.embed {
+                    Group {
+                        switch embed {
+                        case .type0(let images):
+                            ForEach (images.images, id: \.fullsize) { image in
+                                AsyncImage(url: URL(string: image.thumb)){ image in
+                                    image.resizable()
+                                } placeholder: {
+                                    Color.gray
+                                }
+                                .frame(width: .infinity)
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                                .padding(.trailing, 0.5)
+                            }
+                        case .type1(let external):
+                            Text(external.external.uri)
+                        case .type2(let record):
+                            Text("2")
+                        case .type3(let recordWithMedia):
+                            Text("3")
+                        }
+                    }
+                }
+                
+                HStack {
                     Label("Reply", systemImage: "bubble.left")
                         .labelStyle(.iconOnly)
-                    
-                    Label(String(feedPost.post.repostCount ?? 0), systemImage: "repeat")
-                    
-                    Label(String(feedPost.post.likeCount ?? 0), systemImage: "heart")
-                        .symbolVariant(feedPost.post.viewer?.like == nil ? .none : .fill).foregroundColor(.)
+                        .frame(minWidth: 25.0, alignment: .leading)
+                        .padding(.trailing)
+                    HStack {
+                        Label("Repost", systemImage: "repeat")
+                            .labelStyle(.iconOnly)
+                        Text(String(feedPost.post.repostCount ?? 0))
+                    }.onTapGesture {
+                        repostSheetOpen = true
+                    }.accessibilityLabel("Opens the repost/quote post menu")
+                        .confirmationDialog("Repost or quote post",isPresented: $repostSheetOpen, titleVisibility: .hidden) {
+                            Button("Repost") {
+                                print("repost")
+                            }
+                            Button("Quote post") {
+                                print("quote post")
+                            }
+                        }
+                        .frame(minWidth: 60.0, alignment: .leading)
+                        .padding(.trailing)
+                    HStack {
+                        Label("Like", systemImage: "heart")
+                            .symbolVariant(feedPost.post.viewer?.like == nil ? .none : .fill)
+                            .labelStyle(.iconOnly)
+                        Text(String(feedPost.post.likeCount ?? 0))
+                    }
                     
                 }
             }
